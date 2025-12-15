f28a09567adc43fe3d2ead4e3757d385131d178a
Calamity <calamity_xemus@hotmail.com>
2025-12-15 13:18:52 -0700

    removing subtree for conversion to submodules

--- nvim/.config/nvim/lua/utils/init.lua
+++
@@ -1,6 +0,0 @@
-require("utils.list_vim_global")
-require("utils.pluginHeader")
-require("utils.function_list")
-
---  --------- Basic Utility Functions ---------------------
-require("utils.toggle_whichkey")
